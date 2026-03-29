b85080ef4ae67afa19d674edcbe4f78a54c2a15b
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-03-29 16:24:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-IASR
+#include <stdio.h>
 #include "fun.h"
-
 int main() {
+    const char* test_str = "Hello World idk k1ll l0v3 fr0m The ASHES Why Tell";
+    printf("First res: %u\n", faStr1(test_str));
+    printf("Second res: %u\n", faStr2(test_str));
+    printf("First res: %u\n", faStr3(test_str));
     return 0;
 }
