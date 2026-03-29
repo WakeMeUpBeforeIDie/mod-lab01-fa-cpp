3d38bf942317a4a9122cbf2c01a5584b0c071f97
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-03-29 14:50:01 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+using namespace std;
 
 int main() {
+    const* char test_str = "Hello World 123 damn l0v3 d1e r1s3 fr0m th3 Ashes Again. Malevola Invisigal RimmiR .! oOoOoOo";
+
+    cout << "Result of first func: " << faStr1(test_str) << endl;
+    cout << "Result of second func: " << faStr2(test_str) << endl;
+    cout << "Result of third func: " << faStr3(test_str) << endl;
+
     return 0;
 }
