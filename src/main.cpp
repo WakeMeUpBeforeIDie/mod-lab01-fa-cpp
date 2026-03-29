26fb671b1ad1d271e81b5d5323b1de1f46b43677
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-03-29 15:43:23 +0300

    Update 2 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-
+#include <iostream>
 int main() {
+    const* char str = "Hello World idk k1ll l0v3 d13 R153 again fr0m The ASHES 12345 0o0o0o0 OoOoOo Ooooo why Why Tell Me";
+    std::cout << "First func res: " << faStr1(str) << endl;
+    std::cout << "Second func res: " << faStr2(str) << endl;
+    std::cout << "Third func res: " << faStr3(str) << endl;
     return 0;
 }
