14e3804ea0866cad974d8f774a7271d1b16f72a0
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-03-29 15:59:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include <stdio.h>
-#include <string>
 #include "fun.h"
-int main() { 
-    std::string str = "Hello World idk k1ll l0v3 fr0m The ASHES Why Tell";
-    printf("First res: %u\n", faStr1(str));
-    printf("Second res: %u\n", faStr2(str));
-    printf("First res: %u\n", faStr3(str));
+int main() {
+    const char* test_str = "Hello World idk k1ll l0v3 fr0m The ASHES Why Tell";
+    printf("First res: %u\n", faStr1(test_str));
+    printf("Second res: %u\n", faStr2(test_str));
+    printf("First res: %u\n", faStr3(test_str));
     return 0;
 }
