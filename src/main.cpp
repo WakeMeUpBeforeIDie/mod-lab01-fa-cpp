7e7c6544a1985cc5a1a2ccefb40750ed52758e5a
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-03-29 15:54:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "fun.h"
 int main() {
-    const* char str = "Hello World idk k1ll l0v3 fr0m The ASHES Why Tell";
+    string str = "Hello World idk k1ll l0v3 fr0m The ASHES Why Tell";
     printf("First res: %u\n", faStr1(str));
     printf("Second res: %u\n", faStr2(str));
     printf("First res: %u\n", faStr3(str));
